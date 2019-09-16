689d455678d3d2d9d23dba475ba396169d96a5f6
Mathias Nielsen <mathias.l.nielsen@hotmail.dk>
2019-09-16 18:15:17 +0200

    React environment setup done

---
+++ cafe-analog/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Let the Café Analog website development begin
+        <p>16. September 2019</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
